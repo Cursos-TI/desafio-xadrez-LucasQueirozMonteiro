7bdf0c5487f2e9466ad711f24b362103ab102803
LucasQueirozMonteiro <lucasqm2001@gmail.com>
2025-03-31 03:06:13 +0000

    xadresAventureiro

---
+++ xadresAventureiro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 0, j = 0;
+
+    printf("Movimento do Cavalo: \n");
+
+    for ( i = 0; i < 1; i++)
+    {
+        
+        do
+        {
+            printf("Baixo\n");
+            j++;
+        } while (j <= 1);
+        printf("Esquerda\n");
+    }
+    
+}
